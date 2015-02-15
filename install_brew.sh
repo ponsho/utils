f5c6d0621cbd67b9a081b683cbf0062202cbaf2b
poncho <poncho@ponchos-MacBook-Pro.local>
2015-02-14 23:31:39 -0800

    Adding brew

---
+++ install_brew.sh
@@ -0,0 +1,4 @@
+# From http://brew.sh/
+# Brew may need Xcode, you can get it from the AppStore while installing brew
+#!/bin/bash
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
